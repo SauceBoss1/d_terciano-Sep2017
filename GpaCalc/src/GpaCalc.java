39f77bd582e5810804af0508a19bad3394dd9f79
SauceBoss1 <dtvsworld@gmail.com>
2017-09-03 18:34:41 -0700

    Gpa Calculator Calculates gpa

--- GpaCalc/src/GpaCalc.java
+++ GpaCalc/src/GpaCalc.java
@@ -1,4 +1,12 @@
-import java.util.Scanner; 
+import java.util.Scanner;
+
+/*
+ * @author DerfelTercian
+ * @version 0.1
+ * This calculates the gpa of a person
+ * 
+ * 
+ */
 public class GpaCalc {
 	public static double averageGrade(double grade1,double grade2,double grade3) {
 		double x = (grade1+grade2+grade3)/3;
@@ -8,9 +16,9 @@ public class GpaCalc {
 		System.out.println(averageGrade(grade1,grade2,grade3));
 	}
 	public static void main(String[] args) {
-		double grade1= 4.0;
-		double grade2= 3.0;
-		double grade3= 2.0;
+		double grade1=4.0;
+		double grade2=4.1;
+		double grade3=4.0;
 		printAverage(grade1,grade2,grade3);
 	}
 
