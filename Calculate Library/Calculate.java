d163d332eefc258d5e10910a59115f4a3de15f8f
SauceBoss1 <dtvsworld@gmail.com>
2017-09-05 08:37:52 -0700

    Calculate Library
    Stores the calculations.

--- Calculate Library/Calculate.java
+++ Calculate Library/Calculate.java
@@ -1,5 +1,9 @@
 /*
- * does math
+ * @author derfelTerciano
+ * @version 0.1
+ * 
+ * does math and operations
+ * does not actually print anything.
  */
 public class Calculate {
 	public static int square(int x) {
