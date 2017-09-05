f00604fef6b94a70cd758675716d2bfefbac4609
SauceBoss1 <dtvsworld@gmail.com>
2017-09-05 08:35:58 -0700

    Calculator
    Calculates the actual math.

--- Acess Calculate/DoMath.java
+++ Acess Calculate/DoMath.java
@@ -1,8 +1,14 @@
-
+/*
+ * @author derfelTerciano
+ * @version 0.1
+ * This prints out the calculations 
+ */
 public class DoMath {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int x= Calculate.square(3);
+		System.out.println(x);
 
 	}
 
