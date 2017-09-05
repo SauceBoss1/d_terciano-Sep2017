161ac9e7a9d11e6e6178d265acc8b886dba886fc
SauceBoss1 <dtvsworld@gmail.com>
2017-09-05 09:24:15 -0700

    Calculate library
    Store operation of calculator

--- Calculate Library/Calculate.java
+++ Calculate Library/Calculate.java
@@ -10,5 +10,23 @@ public class Calculate {
 		return x*x;
 		
 	}
+	public static int cube(int num) {
+		return num*num*num;
+	}
+	public static double average(double num1 , double num2 ) {
+		return (num1+num2)/2;
+	}
+	public static double average(double num1,double num2,double num3) {
+		return (num1+num2+num3)/3;
+	}
+	public static double toDegrees(double rad) {
+		return (180*rad)/3.14159;
+	}
+	public static double toRadians(double degrees) {
+		return (degrees*3.14159)/180;
+	}
+	public static double discrminant(double a, double b, double c) {
+		return (b*b)-(4*a*c);
+	}
 
 }
