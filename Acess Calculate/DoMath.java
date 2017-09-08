ecf1da3a2c7c260c96cbf2b019623ffb54cef210
SauceBoss1 <dtvsworld@gmail.com>
2017-09-07 17:40:10 -0700

    Calculator
    Calculates a users input

--- Acess Calculate/DoMath.java
+++ Acess Calculate/DoMath.java
@@ -6,8 +6,7 @@
 public class DoMath {
 
 	public static void main(String[] args) {
-		System.out.println(Calculate.toDegrees(3));
-		System.out.println(Calculate.foil(1, 2, 1, 2, "f"));
+		System.out.println(Calculate.min(-15,10));
 	}
 
 }
