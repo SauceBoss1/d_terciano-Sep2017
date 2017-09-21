6517bc05dc7e4c0c3e723b9399f801c7efe96a0d
SauceBoss1 <dtvsworld@gmail.com>
2017-09-20 17:41:07 -0700

    Practiceit work
    This has practice it work.

--- Practiceit/src/Practiceit.java
+++ Practiceit/src/Practiceit.java
@@ -1,8 +1,9 @@
+import java.lang.StringBuilder;
 
 public class Practiceit {
 	public static int gcd(int a, int b) {
-		a = (a>0)? a:-a;
-		b = (b>0)? b:-b;
+		a = (a > 0) ? a : -a;
+		b = (b > 0) ? b : -b;
 		int out = 0;
 		if (a != 0 && b != 0) {
 			for (int i = 1; i <= a && i <= b; i++) {
@@ -20,8 +21,32 @@ public class Practiceit {
 		}
 	}
 
+	public static String repl(String a, int x) {
+		if (x <= 0) {
+			return "";
+		}
+		StringBuilder output = new StringBuilder();
+		for (int i = 0; i <= x; i++) {
+			output.append(a);
+		}
+		return output.toString();
+	}
+
+	public static String season(int month, int day) {
+		if ((month > 3 && month < 6) || (month == 3 && day >= 16) || (month == 6 && day == 15)) {
+			return "Spring";
+		} else if ((month > 12 && month < 3) || (month == 12 && day == 16) || (month == 3 && day == 15)) {
+			return "Winter";
+
+		}else if ((6 < month && month < 9) || (month == 6 && day >= 16) || (month == 9 && day <= 15)) {
+			return "Summer";
+		} else {
+			return "Fall";
+		}
+	}
+
 	public static void main(String[] args) {
-		System.out.println(gcd(-512, -12));
+		System.out.println(season(12,25));
 	}
 }
 
