d24f9212c2cc3607790d68015229a5a4103befbd
SauceBoss1 <dtvsworld@gmail.com>
2017-09-22 18:32:04 -0700

    Calculator
    Calculates the input

--- Acess Calculate/DoMath.java
+++ Acess Calculate/DoMath.java
@@ -6,7 +6,7 @@
 public class DoMath {
 
 	public static void main(String[] args) {
-		System.out.println(Calculate.quadForm(3,-4,-5));
+		System.out.println(Calculate.quadForm(5,-45,-51));
 		
 	}
 
