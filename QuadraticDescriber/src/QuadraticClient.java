7f5a9a6df924ee02a3a29c3a6505c1f3a7e2df7d
SauceBoss1 <dtvsworld@gmail.com>
2017-09-26 15:16:58 -0700

    Quadratic Describer
    Describes a quadratic with the user defined inputs.

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -2,6 +2,12 @@ import java.util.*;
 public class QuadraticClient {
 	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) {
+		String intro_message= "Welcome to this fancy Quadratic describer!\nThis program helps describe what a quadratic would look like if you tell me 3 different coefficents";
+		String intro_message2="All inputs will be formated to \"ax^2+bx+c\"";
+		String author="Program created by Derfel Terciano.";
+		System.out.println(intro_message);
+		System.out.println(intro_message2);
+		System.out.println("\n"+author+"\n");
 		boolean stop = false;
 		while (stop==false) {
 			System.out.print("Enter value a:");
@@ -15,6 +21,7 @@ public class QuadraticClient {
 			System.out.println("\n\nDo you wish to continue??(y/n)");
 			String decide = input.next();
 			if ((decide.equals("n"))) {
+				System.out.println("\nThank you for using this program!");
 				stop = true;
 			}
 			
