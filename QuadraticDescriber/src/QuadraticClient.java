59a20e72977c4b98b244a4885754b2930efd0926
SauceBoss1 <dtvsworld@gmail.com>
2017-10-02 08:13:17 -0700

    QuadraticDescriber
    Describes a quadratic with 3 given coefficents

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -10,6 +10,7 @@ public class QuadraticClient {
 	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) {
 		//this part is not that important
+		//this just creates a welcome message
 		String intro_message= "Welcome to this fancy Quadratic describer!\nThis program helps describe what a quadratic would look like if you tell me 3 different coefficents";
 		String intro_message2="All inputs will be formated to \"ax^2+bx+c\"";
 		String author="Program created by Derfel Terciano.\n";
